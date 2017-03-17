ec2975e4e206181176069cf740aa47da3c8dcd43
Andreza Feu <andrezafeu@gmail.com>
2017-03-16 21:34:28 -0400

    Add function to print message

---
+++ app.js
@@ -0,0 +1,4 @@
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript.`;
+  console.log(message);
+}
