bf1b1aadd524f7066780208f75ae1cb32b00999b
Andreza Feu <andrezafeu@gmail.com>
2017-04-29 13:20:28 -0400

    Catch bad arguments error

--- app.js
+++ app.js
@@ -7,25 +7,29 @@ function printMessage(username, badgeCount, points) {
 }
 
 function getProfile(username) {
-    // Connect to the API URL (https://teamtreehouse.com/username.json)
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-                                // console.log('status:', response.statusCode);
-                                // Read the data
-                                let body = "";
-                                // A data event in node.js is an end event
-                                response.on('data', data => {
-                                    body += data.toString();
-                                });
-                                // Implement end handler
-                                response.on('end', () => {
-                                    // Parse the data
-                                    const profile = JSON.parse(body)
-                                    // console.dir(profile);
-                                    printMessage(username, profile.badges.length, profile.points.JavaScript)
-                                });
+    try {
+        // Connect to the API URL (https://teamtreehouse.com/username.json)
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+                                    // console.log('status:', response.statusCode);
+                                    // Read the data
+                                    let body = "";
+                                    // A data event in node.js is an end event
+                                    response.on('data', data => {
+                                        body += data.toString();
+                                    });
+                                    // Implement end handler
+                                    response.on('end', () => {
+                                        // Parse the data
+                                        const profile = JSON.parse(body)
+                                        // console.dir(profile);
+                                        printMessage(username, profile.badges.length, profile.points.JavaScript)
+                                    });
 
-                              });
-    request.on('error', error => console.error(`Problem with request: ${error.message}`))
+                                  });
+        request.on('error', error => console.error(`Problem with request: ${error.message}`))
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 // Slice the array starting on index 2. This is done to discard the first 2 args in the command line.
