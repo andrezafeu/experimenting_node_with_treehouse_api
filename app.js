a9edaa3fc5c8f4a13132cf0eb22dee05e719d848
Andreza Feu <andrezafeu@gmail.com>
2017-04-28 21:30:21 -0400

    Require https module and return status code from treehouse api

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// Require https module
+const https = require('https');
+const username = "chalkers";
+
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript.`;
   console.log(message);
 }
+
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+                            console.log(response.statusCode);
+                          });
