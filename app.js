6b14767cb28d17b34852063c6256a59e9bd9cc7f
Andreza Feu <andrezafeu@gmail.com>
2017-04-29 00:48:03 -0400

    Use args from command line on function getProfile

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ function printMessage(username, badgeCount, points) {
 }
 
 function getProfile(username) {
-
     // Connect to the API URL (https://teamtreehouse.com/username.json)
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-                                console.log('status:', response.statusCode);
+                                // console.log('status:', response.statusCode);
                                 // Read the data
                                 let body = "";
                                 // A data event in node.js is an end event
@@ -28,4 +27,12 @@ function getProfile(username) {
                               });
 }
 
-getProfile("chalkers");
+// Slice the array starting on index 2. This is done to discard the first 2 args in the command line.
+// For example, for the command '$ node app.js chalkers alenaholligan davemcfarland' it would discard node and app.js
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
+// short form of:
+// users.forEach(username => {
+//     getProfile(username);
+// })
